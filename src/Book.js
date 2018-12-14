b6a3cf2c9d599a12036ad7a61a8fd708662f222d
Varsha Suryawanshi <varshasuryawanshi02@gmail.com>
2018-12-14 23:05:47 +0530

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,1 +1,46 @@
 
+
+import React, {Component} from 'react'
+import {PropTypes} from 'prop-types'
+
+class Book extends Component {
+
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onUpdate: PropTypes.func.isRequired
+  }
+
+  changeShelf = (e) => {
+    this.props.onUpdate(e.target.value)
+  }
+
+  render() {
+    const book = this.props.book
+    return (
+      <li>
+      <div className="book">
+      <div className="top">
+      <div className="cover" style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${book.imageLinks.thumbnail}")`
+            }}></div>
+       <div className="bookShelfChanger">
+              <select onChange={this.changeShelf} value={book.shelf}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="booktitle">{book.title}</div>
+          <div className="bookauthors">{book.authors}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book;
